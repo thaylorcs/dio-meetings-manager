dd984989c2ecdeace4634d01aa8a1858e86ab39f
Thaylor Carvalho <thaylorcs@outlook.com>
2020-11-18 17:34:44 -0300

    Início do FrontEnd

--- Backend/src/main/java/com/dio/meetings/meetings/controller/RoomController.java
+++ Backend/src/main/java/com/dio/meetings/meetings/controller/RoomController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class RoomController {
 
     @Autowired
