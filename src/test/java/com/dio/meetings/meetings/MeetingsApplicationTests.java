346252caeddd335b9d0488892758275bca503987
Thaylor Carvalho <thaylorcs@outlook.com>
2020-11-18 11:53:16 -0300

    Início do projeto

---
+++ src/test/java/com/dio/meetings/meetings/MeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.meetings.meetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
